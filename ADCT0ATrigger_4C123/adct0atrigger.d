d17d76af59e140522eba4155fa4a8a5dcce932f7
Ayan Basu <45741394+ayanbasu11@users.noreply.github.com>
2021-05-07 18:35:19 -0500

    Add files via upload

---
+++ ADCT0ATrigger_4C123/adct0atrigger.d
@@ -0,0 +1,3 @@
+.\adct0atrigger.o: ADCT0ATrigger.c
+.\adct0atrigger.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\adct0atrigger.o: ../inc/tm4c123gh6pm.h
---
+++ ADCT0ATrigger_4C123/adctestmain.d
@@ -0,0 +1,5 @@
+.\adctestmain.o: ADCTestmain.c
+.\adctestmain.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\adctestmain.o: ../inc/tm4c123gh6pm.h
+.\adctestmain.o: ADCT0ATrigger.h
+.\adctestmain.o: PLL.h
